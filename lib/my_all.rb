3d3aac246e22d70ec4952246e499f6aad5b49a25
Malcom Fetus <sora.vista@gmail.com>
2020-02-23 22:34:07 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,7 +8,7 @@ block_return_values = []
     block_return_values << yield(collection[i])
     i = i + 1
 
-if block_return_values.include? (true)
+if block_return_values.include? 
   false
 else
   true
