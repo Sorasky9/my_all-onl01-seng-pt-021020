2139b03fdbd4964aee7a45a6b6a7ee74983c82ff
Malcom Fetus <sora.vista@gmail.com>
2020-02-23 22:21:10 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+block_return_values = []
+  while i < collection.length
+
+    block_return_values << yield(collection[i])
+    i = i += 1
+
+if block_return_values.include?(false)
+  false
+else
+  false
+  end
+
+end
+return block_return_values
 
 end
