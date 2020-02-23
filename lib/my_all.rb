8983895c1e35f67e966b848d9736de92b439070e
Malcom Fetus <sora.vista@gmail.com>
2020-02-23 22:30:54 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,7 +6,7 @@ block_return_values = []
   while i < collection.length
 
     block_return_values << yield(collection[i])
-    i = i += 1
+    i = i + 1
 
 if block_return_values.include?(false)
   false
